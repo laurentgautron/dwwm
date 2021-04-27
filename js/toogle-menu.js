4a7ca9e2ae900cb60317ef04746999160fcac82d
laurent GAUTRON <laurentgautron@protonmail.com>
2021-04-27 22:21:04 +0200

    mis un mot dans le burger menu

--- js/toogle-menu.js
+++ js/toogle-menu.js
@@ -1,8 +1,10 @@
 const toggleMenu = document.querySelector('.toggle-menu');
 const headerMenu = document.querySelector('.header-menu');
+const burger = document.querySelector('burger');
 
 toggleMenu.addEventListener('click', function() {
     const rolling = JSON.parse(toggleMenu.getAttribute('aria-expanded'));
     toggleMenu.setAttribute('aria-expanded', !rolling);
+    document.body.classList.toggle('cross');
     headerMenu.hidden = !headerMenu.hidden;
 })
