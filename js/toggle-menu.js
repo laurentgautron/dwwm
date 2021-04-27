592e156166fb7aed60678b2b33b7870290e2d72c
laurent GAUTRON <laurentgautron@protonmail.com>
2021-04-27 23:28:17 +0200

    burger menu : étoile terminé

--- js/toggle-menu.js
+++ js/toggle-menu.js
@@ -1,6 +1,5 @@
 const toggleMenu = document.querySelector('.toggle-menu');
 const headerMenu = document.querySelector('.header-menu');
-const burger = document.querySelector('burger');
 
 toggleMenu.addEventListener('click', function() {
     const rolling = JSON.parse(toggleMenu.getAttribute('aria-expanded'));
